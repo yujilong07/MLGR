b8a42cc19b9fbaa1f9deca46b1e4f990e7bc491e
Meilong <yujilong07@gmail.com>
2026-05-04 10:54:38 +0200

    user sch

---
+++ backend/app/schemas/user.py
@@ -0,0 +1,20 @@
+from pydantic import BaseModel, EmailStr
+from datetime import datetime
+
+class UserCreate(BaseModel):
+    username: str
+    email: EmailStr
+    password: str
+
+class UserResponse(BaseModel):
+    id: int
+    username: str
+    email: str
+    created_at: datetime
+
+    class Config:
+        from_attributes = True
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
