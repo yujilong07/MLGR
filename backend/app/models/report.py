d202d4120ddfe4fddb4c4e26807125bc25f43de6
Meilong <yujilong07@gmail.com>
2026-05-04 10:54:14 +0200

    models

---
+++ backend/app/models/report.py
@@ -0,0 +1,18 @@
+from sqlmodel import SQLModel, Field
+from datetime import datetime
+from typing import Optional
+from sqlalchemy import Column
+from sqlalchemy.dialects.postgresql import JSON
+
+class Report(SQLModel,table = True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    user_id: int = Field(foreign_key="user.id")
+    title :str
+    discipline: str
+    teacher : str
+    group: str
+    goal: str
+    sections : Optional[dict] = Field(default=None,sa_column=Column(JSON))
+    conclusion : Optional[str] = None
+    created_at: datetime = Field(default_factory=datetime.utcnow)
+    updated_at: datetime = Field(default_factory=datetime.utcnow)
---
+++ backend/app/models/user.py
@@ -0,0 +1,11 @@
+from sqlmodel import SQLModel, Field
+from datetime import datetime
+from typing import Optional
+
+
+class User(SQLModel,table = True):
+    id: Optional[int] = Field(default = None, primary_key = True)
+    email: str = Field(unique=True, index=True)
+    username : str = Field(unique=True, index=True)
+    hashed_pass: str
+    created_at: datetime = Field(default_factory=datetime.utcnow)
